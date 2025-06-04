1e43a52c29ce6c4c22d8329552fe3bdc02fcffbb
Renzo Diaz <renzodiaz554@gmail.com>
2025-06-04 00:13:55 -0500

    feat: missing files for profile commited

---
+++ components/shared/ProfileHeader.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+interface ProfileHeaderProps {
+  accountId: string;
+  authUserId: string;
+  name: string;
+  username: string;
+  imgUrl: string;
+  bio: string;
+}
+
+export const ProfileHeader = (props: ProfileHeaderProps) => {
+  return (
+    <div className="flex w-full flex-col justify-start">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className="relative h-20 w-20 object-cover">
+            <Image src={props.imgUrl} alt="Profile Image" fill className="rounded-full object-cover shadow-2xl" />
+          </div>
+          <div className="flex-1">
+            <h2 className="text-left text-heading3-bold text-light-1">{props.name}</h2>
+            <p className="text-base-medium text-gray-1">@{props.username}</p>
+          </div>
+        </div>
+
+        {/* {TODO: Community} */}
+      </div>
+      <p className="mt-6 max-w-lg text-base-regular text-light-2">{props.bio}</p>
+      <div className="mt-12 h-0.5 w-full bg-dark-3" />
+    </div>
+  );
+};
---
+++ components/shared/WispsTab.tsx
@@ -0,0 +1,39 @@
+import { getUserWisps } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import { WispCard } from "../cards/WispCard";
+
+interface WispsTabProps {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+export default async function WispsTab({ currentUserId, accountId, accountType }: WispsTabProps) {
+  let result = await getUserWisps(currentUserId);
+
+  if (!result) {
+    redirect("/");
+  }
+
+  return (
+    <section className="mt-9 flex flex-col gap-10">
+      {result.wisps.map((wisp: any) => (
+        <WispCard
+          key={wisp._id}
+          id={wisp._id}
+          currentUserId={currentUserId}
+          parentId={wisp.parentId}
+          content={wisp.text}
+          author={
+            accountType === "User"
+              ? { name: result.name, image: result.image, id: result.id }
+              : { name: wisp.author.name, image: wisp.author.image, id: wisp.author.id }
+          }
+          community={wisp.community} //TODO
+          createdAt={wisp.createdAt}
+          comments={wisp.children}
+        />
+      ))}
+    </section>
+  );
+}
