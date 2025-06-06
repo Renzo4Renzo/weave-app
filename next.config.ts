b1a7dd3d62c5871ca92b0b3d1928138b3893f593
Renzo Diaz <renzodiaz554@gmail.com>
2025-06-05 19:04:21 -0500

    feat: removing comments in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   serverActions: {},
-  //   serverComponentsExternalPackages: ["mongoose"],
-  // },
   images: {
     remotePatterns: [
       {
